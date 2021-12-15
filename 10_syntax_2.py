27003a68d49f54c3d11a18a6b33bb45abb412715
Kate Tester <katemorris@gmail.com>
2021-12-14 20:26:17 -0700

    finish day 10

--- 10_syntax_2.py
+++ 10_syntax_2.py
@@ -1,3 +1,5 @@
+from statistics import *
+
 def read_chunks(file):
     open_file = open(file, 'r')
     chunks = open_file.read().splitlines()
@@ -20,18 +22,46 @@ def find_first_error(string):
                 opens.pop(-1)
             else:
                 return char
+    return opens
 
 def get_closers_score(opens):
+    reversed_list = list(reversed(opens))
+    closers = []
+    for char in reversed_list:
+        if char == '<':
+            closers.append('>')
+        elif char == '(':
+            closers.append(')')
+        elif char == '{':
+            closers.append('}')
+        elif char == '[':
+            closers.append(']')
 
+    total = 0
+    for char in closers:
+        total *= 5
+        if char == ')':
+            total += 1
+        elif char == ']':
+            total += 2
+        elif char == '}':
+            total += 3
+        elif char == '>':
+            total += 4
+
+    return total
 
 def get_syntax_error_score(file):
     data = read_chunks(file)
-    errors = []
+    totals = []
     for line in data:
         result = find_first_error(line)
-        if type(result) == 'str' and result in [')', ']','}','>']:
+        if isinstance(result, str) and result in [')', ']','}','>']:
             continue
         else:
-            get_closers(result)
+            totals.append(get_closers_score(result))
+    totals.sort()
+    #import code; code.interact(local=dict(globals(), **locals()))
+    return median(totals)
 
 print(get_syntax_error_score('files/day10.txt'))
